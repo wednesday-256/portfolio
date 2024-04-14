abbb41958db46cca48518df865e7a04d537a9dad
Felix Wednesday <felix.dido.4@gmail.com>
2024-04-14 06:35:38 +0100

    added image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import vsharp from "vite-plugin-vsharp";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio/"
-})
+  plugins: [react(), vsharp()],
+  base: "/portfolio/",
+});
